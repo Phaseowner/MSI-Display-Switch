7cd4938b0fff46b677d020fe7b16abb431032ba6
Phaseowner <phaseowner@gmail.com>
2025-01-08 23:41:06 +0700

    Initial Commit

---
+++ MSI Display Switch/MSI_Display_SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSI_Display_SwitchApp.swift
+//  MSI Display Switch
+//
+//  Created by Валерий Агишев on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MSI_Display_SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
